422c72673c13e1dfe7f9015b7021e4ec94e99bc6
Brown Sidis <brownakajameh@gmail.com>
2022-06-30 09:44:19 -0700

    0. Preprocessor

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -18,28 +18,3 @@ int main(void)
 
 }
 
-root@8b5d7708469a:/alx-low_level_programming/0x00-hello_world# export CFILE=main.c
-
-root@8b5d7708469a:/alx-low_level_programming/0x00-hello_world# ./0-preprocessor 
-
-root@8b5d7708469a:/alx-low_level_programming/0x00-hello_world# tail c
-
-# 942 "/usr/include/stdio.h" 3 4
-
-
-
-# 2 "main.c" 2
-
-
-
-
-
-# 3 "main.c"
-
-int main(void)
-
-{
-
-	 return (0);
-
-}
