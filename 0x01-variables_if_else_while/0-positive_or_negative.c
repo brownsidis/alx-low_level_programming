07c223e6d97356c168d1691a4f1ddeeb8bdbbe2c
Brown Sidis <94837053+brownsidis@users.noreply.github.com>
2022-07-01 21:54:38 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,10 +8,10 @@
  *
  * Return: Always 0 (Success)
  */
+ 
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -26,6 +26,5 @@ int main(void)
 	{
 		printf("%i is negative\n", n);
 	}
-
 	return (0);
-}
+	}
