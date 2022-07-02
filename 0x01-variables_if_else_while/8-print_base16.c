2ced32f70da340b2f93b2db91e00fcdfeaf85772
Brown Sidis <94837053+brownsidis@users.noreply.github.com>
2022-07-02 04:33:17 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block to be executed
+ * Description: printing base 10 digits
+ * Return: 0 Success
+ */
+int main(void)
+{
+char ch;
+for (ch = '0'; ch <= '9'; ch++)
+{
+	putchar(ch);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
