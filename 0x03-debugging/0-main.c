db01ff783d821fade817400c7d0cb634618ade8f
Brown Sidis <94837053+brownsidis@users.noreply.github.com>
2022-07-07 13:53:17 -0400

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,2 +1,16 @@
-0x03. C - Debugging
+#include "main.h"
 
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
